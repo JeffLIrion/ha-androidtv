17d60a959a6134abaf4d6f80476d562121707074
Jeff Irion <jefflirion@users.noreply.github.com>
2019-06-22 15:51:19 -0700

    Remove 'get_androidtv.sh' script

--- get_androidtv.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# androidtv
-rm -rf custom_components/androidtv
-mkdir -p custom_components/androidtv
-wget -O custom_components/androidtv/__init__.py https://raw.githubusercontent.com/home-assistant/home-assistant/dev/homeassistant/components/androidtv/__init__.py
-wget -O custom_components/androidtv/manifest.json https://raw.githubusercontent.com/home-assistant/home-assistant/dev/homeassistant/components/androidtv/manifest.json
-wget -O custom_components/androidtv/media_player.py https://raw.githubusercontent.com/home-assistant/home-assistant/dev/homeassistant/components/androidtv/media_player.py
-wget -O custom_components/androidtv/services.yaml https://raw.githubusercontent.com/home-assistant/home-assistant/dev/homeassistant/components/androidtv/services.yaml
